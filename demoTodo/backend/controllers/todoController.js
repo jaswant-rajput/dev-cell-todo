4afa4d4a27ad2240280e586bd03bbead06964846
unknown <admin@31D-LAB3-02.SVV.local>
2023-12-20 09:48:19 +0530

    frontend added

--- demoTodo/backend/controllers/todoController.js
+++ demoTodo/backend/controllers/todoController.js
@@ -1,4 +1,5 @@
 
+const { response } = require("express");
 const Todo = require("../models/todoSchema")
 
 exports.createNote = (req,res) => {
@@ -16,8 +17,90 @@ exports.createNote = (req,res) => {
               status: "fail",
               message : err
             });
-            
+        })   
+}
+
+exports.getAllTodos = (req,res)=>{
+  
+    Todo.find().then(response=>{
+      res.json({
+        status:"success",
+        data:response
+      })
+    }).catch(err => {
+      res.json({
+        status:"fail",
+        message:err
+      })
+    })
+
+}
+
+exports.getTodoById = (req,res) =>{
+  Todo.findById(req.params.todoId).then(response => {
+    res.json({
+      status:"success",
+      data : response
+    })
+  }).catch(err =>{
+    res.json({
+      status:"fail",
+      message:err
+    })
+  })
+}
+
+exports.getTodoByStart = (req,res) =>{
+  const start = req.params.start
+  Todo.find().then(data=>{
+    const response = []
+  
+    const regex = new RegExp(start);
+    data.map(d => {
+      if (regex.test(d.title)){
+        response.push(d)
+      }
+    })
+
+    res.json({
+      status:"success",
+      data :response
+    })
+  }).catch(err =>{
+    res.json({
+      status:"success",
+      data :err
+    })
+  })
+}
+
+exports.deleteById = (req,res)=>{
+  Todo.findByIdAndDelete(req.params.todoId).then(response =>{
+        res.json({
+          status:"success",
+          data :response
         })
-          
     
+      }).catch(err =>{
+        
+        res.json({
+          status:"success",
+          data :err
+        })
+      })
+    }
+    
+    exports.updateById = (req,res) =>{
+      Todo.findByIdAndUpdate(req.params.todoId,req.body,{new:true}).then(response =>{
+      res.json({
+        status:"success",
+        data :response
+      })
+    
+  }).catch(err =>{
+    res.json({
+      status:"success",
+      data :err
+    })
+  })
 }
--- demoTodo/backend/routes/todoRoutes.js
+++ demoTodo/backend/routes/todoRoutes.js
@@ -1,15 +1,14 @@
 
 const express = require("express")
-const {createNote} = require("../controllers/todoController")
+const {createNote,getAllTodos,getTodoById,getTodoByStart,deleteById,updateById} = require("../controllers/todoController")
 
 const router = express.Router()
 
 router.post("/create-todo",createNote)
-
-
-// Old 
-// router
-//     .route("/createNote")
-//     .post(todoController.createNote)
-
-module.exports = router
+router.get("/get-all-todo",getAllTodos)
+router.get("/get-todo/:todoId",getTodoById)
+router.get("/get-todo-by-start/:start",getTodoByStart)
+router.delete("/delete-todo/:todoId",deleteById)
+router.put("/update-todo/:todoId",updateById)
+// router.get("/get-todo-by-start/:start",getTodoByStart)
+module.exports = router 
